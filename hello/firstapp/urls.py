d8896e2a1d2d855f729794adce583827eae6ee18
OsamaX01 <shkukanios@gmail.com>
2021-04-01 21:42:02 +0300

    Deleted Hello Project and Created Web Workspace

--- hello/firstapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name = "index"),
-    path("hackerspace", views.hackerspace, name = "hackerspace"),
-    path("<str:name>", views.greet, name = "greet")
-]
