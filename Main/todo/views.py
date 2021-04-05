b64d15c558113e4910f76ea028d11baecb4a1480
OsamaX01 <shkukanios@gmail.com>
2021-04-05 13:54:44 +0300

    added sessions feature to the ToDo app by connecting to defult django database

--- Main/todo/views.py
+++ Main/todo/views.py
@@ -3,15 +3,15 @@ from django.http import HttpResponseRedirect
 from django import forms
 from django.urls import reverse
 
-
-tasks = []
-# db light
 class NewTaskForm(forms.Form):
-    task = forms.CharField(label = "New Task", max_length = 5)
+    task = forms.CharField(label = 'New Task', max_length = 20)
 
 def index(request):
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
+
     return render(request, 'todo/index.html', {
-        "tasks" : tasks
+        'tasks' : request.session['tasks']
     })
 
 def add(request):
@@ -19,7 +19,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session['tasks'] += [task]
             return HttpResponseRedirect(reverse("todo:index"))
         else:
             return render(request, 'todo/add.html', {
